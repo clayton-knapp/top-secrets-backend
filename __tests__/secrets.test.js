28dbdf2ed41e6bf2b15ccb49fdfcb5234a603388
Clayton Knapp <csjknapp@gmail.com>
2022-03-29 19:43:43 -0700

    got rid of console log and unneeded import

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -3,7 +3,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+// const UserService = require('../lib/services/UserService');
 
 describe('top-secrets-backend routes', () => {
   beforeEach(() => {
