8129a4ddaa1b70044d623f923e232bc0ad3b2e00
Clayton Knapp <csjknapp@gmail.com>
2022-03-29 18:18:58 -0700

    stuck trying to make get all secrets work with authenticate

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets-backend routes', () => {
   beforeEach(() => {
@@ -14,7 +15,19 @@ describe('top-secrets-backend routes', () => {
   });
 
 
-  it('returns a list of secrets', async() => {
+  it('returns a list of secrets for logged in user', async() => {
+    //create a user
+    await UserService.hash({
+      email: 'bob@bob.com', 
+      password: 'bobbob'
+    });
+
+    //sign in the user
+    await UserService.signIn(
+      {
+        email: 'bob@bob.com',
+        password: 'bobbob'
+      });
 
     const expected = [
       { 
@@ -36,5 +49,8 @@ describe('top-secrets-backend routes', () => {
 
     expect(res.body).toEqual(expected);
   });
+
+
+  //Logged in users can create new secrets by POSTing to /api/v1/secrets
 
 });
